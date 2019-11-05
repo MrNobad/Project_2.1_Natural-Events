b275195470362e2b5909c05f647818211f7146f0
Laurie Frank <laurie360@hotmail.co.uk>
2019-11-05 10:26:53 +0000

    date fixed

--- src/components/EventDetail.vue
+++ src/components/EventDetail.vue
@@ -1,6 +1,9 @@
 <template lang="html">
   <div v-if="indEvent" id="indEventDetail">
-    <h2>Event Title: {{indEvent.title}}</h2>
+    <h2>Natural Event: {{indEvent.title}}</h2>
+    <p>Type: {{indEvent.categories[0].title}}</p>
+    <p>Date: {{indEvent.geometries[0].date.slice(0, 10)}}</p>
+    <p></p>
   </div>
 </template>
 
@@ -9,6 +12,7 @@ export default {
   name: 'ind-event-detail',
   props: ['indEvent']
 }
+// TODO: use .splice to show date only
 </script>
 
 <style lang="css" scoped>
